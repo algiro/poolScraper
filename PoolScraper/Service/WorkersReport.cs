d2f5f4dd5c51f7cc1a15d2104364e43d027aee74
AleG <ag@audacitycapital.com>
2025-05-03 17:16:56 +0100

    Refactors WorkerReport

--- PoolScraper/Service/WorkersReport.cs
+++ PoolScraper/Service/WorkersReport.cs
@@ -11,46 +11,38 @@ namespace PoolScraper.Service
                 .GroupBy(s => s.WorkerId)
                 .SelectNotNull(group =>
                 {
-                    // Calculate the average hashrate for this group
-                    double weightedHashRateTotal = group.Sum(x => x.Weight() * x.BasicInfo.Hashrate);
-                    double totalWeight = group.Sum(x => x.Weight());
-                    var representative = group.First();
-                    var baseDateFrom = group.Min(g => g.DateRange.From);
-                    var baseDateTo = group.Max(g => g.DateRange.To);
-                    var groupDateRange = DateRange.Create(baseDateFrom, baseDateTo);
-                    var snapshotAverage = SnapshotWorkerStatus.Create(
-                        representative.WorkerId,
-                        Granularity.Custom,
-                        groupDateRange,
-                        WorkerBasicInfo.Create(weightedHashRateTotal / totalWeight, 0)
-                    ) ;
-                    return snapshotAverage.AsSnapshotDetailedView(representative.Worker);
+                    return CreateAverageSnapshot<IWorkerId>(group);
                 });
             return groupedByHourAndWorker;
         }
+
         public IEnumerable<ISnapshotDetailedView> CalculateAveragePerModel(IEnumerable<ISnapshotDetailedView> snapshots)
         {
             var groupedByHourAndWorker = snapshots
                 .GroupBy(s => s.Worker.Model)
                 .SelectNotNull(group =>
                 {
-                    // Calculate the average hashrate for this group
-                    double weightedHashRateTotal = group.Sum(x => x.Weight() * x.BasicInfo.Hashrate);
-                    double totalWeight = group.Sum(x => x.Weight());
-                    var representative = group.First();
-                    var baseDateFrom = group.Min(g => g.DateRange.From);
-                    var baseDateTo = group.Max(g => g.DateRange.To);
-                    var groupDateRange = DateRange.Create(baseDateFrom, baseDateTo);
-                    var snapshotAverage = SnapshotWorkerStatus.Create(
-                        representative.WorkerId,
-                        Granularity.Custom,
-                        groupDateRange,
-                        WorkerBasicInfo.Create(weightedHashRateTotal / totalWeight, 0)
-                    );
-                    return snapshotAverage.AsSnapshotDetailedView(representative.Worker);
+                    return CreateAverageSnapshot<WorkerModel>(group);
                 });
             return groupedByHourAndWorker;
         }
 
+        private static ISnapshotDetailedView? CreateAverageSnapshot<T>(IGrouping<T, ISnapshotDetailedView> group)
+        {
+            // Calculate the average hashrate for this group
+            double weightedHashRateTotal = group.Sum(x => x.Weight() * x.BasicInfo.Hashrate);
+            double totalWeight = group.Sum(x => x.Weight());
+            var representative = group.First();
+            var baseDateFrom = group.Min(g => g.DateRange.From);
+            var baseDateTo = group.Max(g => g.DateRange.To);
+            var groupDateRange = DateRange.Create(baseDateFrom, baseDateTo);
+            var snapshotAverage = SnapshotWorkerStatus.Create(
+                representative.WorkerId,
+                Granularity.Custom,
+                groupDateRange,
+                WorkerBasicInfo.Create(weightedHashRateTotal / totalWeight, 0)
+            );
+            return snapshotAverage.AsSnapshotDetailedView(representative.Worker);
+        }
     }
 }
