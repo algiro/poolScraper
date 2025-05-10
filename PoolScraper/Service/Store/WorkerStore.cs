db7f639e3f680cd0995f77ef2874e6f0dff35378
AleG <ag@audacitycapital.com>
2025-05-10 22:55:41 +0100

    Improves logging on WorkerStore

--- PoolScraper/Service/Store/WorkerStore.cs
+++ PoolScraper/Service/Store/WorkerStore.cs
@@ -3,11 +3,15 @@ using PoolScraper.Persistency;
 
 namespace PoolScraper.Service.Store
 {
-    public class WorkerStore(ILogger<WorkerStore> logger, IWorkerPersistency workerPersistency) : IWorkerStore
+    public class WorkerStore(ILogger logger, IWorkerPersistency workerPersistency) : IWorkerStore
     {
         private IEnumerable<IWorker> _allWorkers = Enumerable.Empty<IWorker>();
         public IEnumerable<IWorker> GetAllWorker() => _allWorkers;
 
+        public WorkerStore(ILogger logger, IEnumerable<IWorker> workers) : this (logger, (IWorkerPersistency) null)
+        {
+            _allWorkers = workers;
+        }
 
         public async Task<IEnumerable<IWorker>> LoadAllWorkerAsync()
         {
@@ -17,7 +21,27 @@ namespace PoolScraper.Service.Store
             return _allWorkers;
         }
 
-        public IWorker? GetById(long id) => _allWorkers.FirstOrDefault(w => w.WorkerId.Id == id);
+        public IWorker? GetById(long id)
+        {
+            try
+            {
+                var worker = _allWorkers.FirstOrDefault(w => w.WorkerId.Id == id);
+                if (worker == null)
+                {
+                    logger.LogWarning("Worker not found fetching from allWorkers#: {workerStore} worker by id: {id}", _allWorkers.Count(), id);
+                }
+                return worker;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError("Error fetching from allWorkers#: {workerStore} worker by id: {id}, error: {error}", _allWorkers.Count(), id, ex.Message);
+                foreach (var worker in _allWorkers)
+                {
+                    logger.LogError("Worker: {worker}", worker.ToString());
+                }
+                return null;
+            }
+        }
         public IEnumerable<IWorker> GetWorkerByAlgo(string algo) => _allWorkers.Where(w => w.Algorithm == algo);
         public IEnumerable<IWorker> GetWorkerByModel(WorkerModel workerModel) => _allWorkers.Where(w => w.Model == workerModel);
 
