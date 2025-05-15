465e6d381a97e1912c5f510a135573a3469d339f
AleG <ag@audacitycapital.com>
2025-05-15 16:37:36 +0100

    Creates DateSeriesExtractor and generic GroupedGraph
    It reuse logic to avoid duplication for each grouping strategy

---
+++ PoolScraper/Components/Utils/DateSeriesExtractor.cs
@@ -0,0 +1,67 @@
+﻿using BlazorBootstrap;
+using PoolScraper.Domain;
+
+namespace PoolScraper.Components.Utils
+{
+    public delegate bool MatchFunc<T>(ISnapshotDetailedView snapshotDetailedView,T compareElement);
+    public delegate IEnumerable<T> GetGroups<T>(IEnumerable<ISnapshotDetailedView> snapData);
+
+    public class DateSeriesExtractor<T>(IEnumerable<ISnapshotDetailedView> snapData, MatchFunc<T> matchFunction, GetGroups<T> GetGroups)
+    {
+        private int datasetsCount = 0;
+        private int labelsCount =0;
+
+        public List<IChartDataset> GetDefaultDataSets()
+        {
+            var datasets = new List<IChartDataset>();
+            var groups = GetGroups(snapData);
+            foreach (var group in groups)
+            {
+                var dataSetForModel = snapData.Where(w => matchFunction(w, group));
+                datasets.Add(GetLineChartDataset(group, dataSetForModel));
+            }
+
+            return datasets;
+        }
+
+        private LineChartDataset GetLineChartDataset<T>(T group, IEnumerable<ISnapshotDetailedView> snapData)
+        {
+            var c = ColorUtility.CategoricalTwelveColors[datasetsCount].ToColor();
+
+            datasetsCount += 1;
+
+            return new LineChartDataset
+            {
+                Label = $"{group}",
+                Data = GetData(snapData),
+                BackgroundColor = c.ToRgbaString(),
+                BorderColor = c.ToRgbString(),
+                PointRadius = new List<double> { 5 },
+                PointHoverRadius = new List<double> { 8 },
+            };
+        }
+
+        private List<double?> GetData(IEnumerable<ISnapshotDetailedView> snapData)
+        {
+            var data = new List<double?>();
+            foreach (var snap in snapData)
+            {
+                data.Add(snap.BasicInfo.Hashrate);
+            }
+
+            return data;
+        }
+
+        public List<string> GetDefaultDataLabels()
+        {
+            var labels = new List<string>();
+            var dateRanges = snapData.Select(w => w.DateRange).Distinct();
+            foreach (var dateRange in dateRanges)
+            {
+                labels.Add(dateRange.From.ToString("yyyy-MM-dd"));
+            }
+
+            return labels;
+        }
+    }
+}
