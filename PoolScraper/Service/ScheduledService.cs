d4617f4d64eb8bb5a4c1edb05f5ab35aa8c3c717
AleG <ag@audacitycapital.com>
2025-06-01 10:38:28 +0100

    Schedules data consolidation
    Schedules the consolidation of user data on a daily basis.
    The consolidation process aggregates data from the past week into daily snapshots.

--- PoolScraper/Service/ScheduledService.cs
+++ PoolScraper/Service/ScheduledService.cs
@@ -2,12 +2,17 @@
 
 using CommonUtils.Utils;
 using PoolScraper.Config;
+using PoolScraper.Domain;
+using PoolScraper.Domain.Consolidation;
+using PoolScraper.Service.Consolidation;
 
 namespace PoolScraper.Service
 {
-    public class ScheduledService(IInitApp initApp, IScrapingServiceClient scrapintServiceClient) : BackgroundService
+    public class ScheduledService(ILogger<ScheduledService> logger, IInitApp initApp, IScrapingServiceClient scrapingServiceClient, ISnapshotConsolidateServiceClient snapshotConsolidateServiceClient) : BackgroundService
     {
         private CancellationToken cancellationToken;
+        private readonly TimeOnly consolidationTime = new TimeOnly(00, 15, 0);
+
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {   
             this.cancellationToken = stoppingToken;
@@ -18,11 +23,37 @@ namespace PoolScraper.Service
         private async Task ScheduleScrapingAsync()
         {
             await TaskUtils.RepeatRun(ImportAction, every: TimeSpan.FromMinutes(1), startAfter: TimeSpan.Zero, cancellationToken: cancellationToken);
+            var timeUntilNextConsolidation = TimeUtils.GetTimeSpanUntilNext(consolidationTime);
+            logger.LogInformation("Next consolidation will run in {timeUntilNextConsolidation}", timeUntilNextConsolidation);
+            await TaskUtils.RepeatRun(ConsolidateAction, every: TimeSpan.FromHours(24), startAfter: timeUntilNextConsolidation, cancellationToken: cancellationToken);
 
         }
+
         private async Task ImportAction()
         {
-            await scrapintServiceClient.FetchAndStoreUserDataAsync();
+            await scrapingServiceClient.FetchAndStoreUserDataAsync();
+        }
+        private async Task ConsolidateAction()
+        {
+            var yesterday = DateUtils.Today.AddDays(-1).GetEndOfDay();
+            var oneWeekBefore = yesterday.AddDays(-7).GetBeginOfDay();
+            logger.LogInformation("Consolidating data from {startDate} to {endDate}", oneWeekBefore, yesterday);
+
+            var dataConsolidated = await snapshotConsolidateServiceClient.GetSnapshotDataConsolidationInfoAsync(DateRange.Create(oneWeekBefore, yesterday));
+            var dayConsolidated = dataConsolidated.Where(d => d.Granularity == Granularity.Days).ToList();
+            var currentDate = DateOnly.FromDateTime(oneWeekBefore);
+            while (currentDate < DateOnly.FromDateTime(yesterday))
+            {
+                var currentDateRange = currentDate.AsDateRange();
+                var hasBeenAlreadyConsolidated = dayConsolidated.Any(d => d.DateRange.Equals(currentDateRange));
+                logger.LogInformation("Consolidating date: {date}, already consolidated: {hasBeenAlreadyConsolidated}", currentDate, hasBeenAlreadyConsolidated);
+                if (!hasBeenAlreadyConsolidated)
+                {
+                    await snapshotConsolidateServiceClient.ConsolidateDays(currentDateRange);
+                    logger.LogInformation("Consolidated dateRange: {currentDateRange}", currentDateRange);
+                }
+                currentDate = currentDate.AddDays(1);
+            }
         }
     }
 }
