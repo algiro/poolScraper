c843a25f42ccf68c0320e3284393fbec77176d0e
AleG <ag@audacitycapital.com>
2025-06-14 03:30:00 +0100

    Automates build and push with versioning
    Adds scripts to build and optionally push Docker images,
    automating the versioning process.
    
    The scripts now support tagging images with either 'latest'
    or a date-based release tag (R.YYYYMMDD) if the 'newRelease'
    parameter is supplied.

--- buildAndPush.cmd
+++ buildAndPush.cmd
@@ -1,2 +1,17 @@
-docker build . -t algiro/pool-scraper:latest
-docker push algiro/pool-scraper:latest
+@echo off
+echo "Usage with no parameter, it creates 'latest' version, with 'newRelease' parameter it creates a new release tag"
+:parse
+set version=latest
+IF not "%~1"=="newRelease" GOTO endparse
+
+for /f %%a in ('wmic os get localdatetime ^| find "."') do set datetime=%%a
+set YYYY=%datetime:~0,4%
+set MM=%datetime:~4,2%
+set DD=%datetime:~6,2%
+set formatted_date=%YYYY%%MM%%DD%
+echo Date: %formatted_date%
+set version=R.%formatted_date%
+:endparse
+echo Version: %version%
+docker build . -t algiro/pool-scraper:%version%
+docker push algiro/pool-scraper:%version%
